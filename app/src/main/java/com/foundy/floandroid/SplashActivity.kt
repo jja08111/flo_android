13682bbcbf458513e57751d3090e43cf83708e4e
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-05-10 22:33:04 +0900

    Implement Splash screen

---
+++ app/src/main/java/com/foundy/floandroid/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.foundy.floandroid
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
