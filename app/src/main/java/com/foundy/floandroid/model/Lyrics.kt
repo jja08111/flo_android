46a32d085ef34f9b97f2029b07a0bfa9c821fcb0
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-05-11 12:39:55 +0900

    밀리초 위치와 가사를 저장하도록 수정 in Lyrics

--- app/src/main/java/com/foundy/floandroid/model/Lyrics.kt
+++ app/src/main/java/com/foundy/floandroid/model/Lyrics.kt
@@ -1,11 +1,26 @@
 package com.foundy.floandroid.model
 
 class Lyrics(raw: String) {
-    private val list: List<Pair<String, String>> = raw.split("\n").map {
+
+    /**
+     * 밀리초와 가사가 페어로 있는 리스트이다.
+     */
+    val timeLyricsPairs: List<Pair<Int, String>> = raw.split("\n").map {
         val splitString = it.split("]")
-        // TODO: 시간을 더 명확히 표현할 수 있도록 클래스 만들어 이용하기
-        val time = splitString[0].removePrefix("[")
+        val time = splitString[0].removePrefix("[").toSecond()
         val lyrics = splitString[1]
         time to lyrics
     }.toList()
+
+    /**
+     * "분:초:밀리초" 로 구성된 문자열을 밀리초 단위로 변환한 것을 반환한다.
+     */
+    private fun String.toSecond(): Int {
+        val splitString = this.split(":")
+        val minutes = splitString[0].toInt()
+        val seconds = splitString[1].toInt()
+        val milliseconds = splitString[2].toInt()
+
+        return minutes * 60 * 1000 + seconds * 1000 + milliseconds
+    }
 }
